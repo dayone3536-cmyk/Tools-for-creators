e4a73ce01bc58552c964ecff6878886634317761
dayone3536-cmyk <dayone3536@gmail.com>
2026-05-19 14:29:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def background_video_editor(input_path, output_path, unique_id):
         # Update status to processing
         video_status_db[unique_id] = "Processing: Running auto-editor..."
         
-        command = f"auto-editor {input_path} --quiet --no-open --output {output_path} --margin 0.4s --video_codec h264 --audio_codec aac"
+        command = f"auto-editor {input_path} --quiet --no-open --output {output_path} --margin 0.2s --video_codec h264 --audio_codec aac"
 
         
         subprocess.run(command, shell=True, check=True)
